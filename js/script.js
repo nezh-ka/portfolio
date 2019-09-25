c3cf92441f30335598e5dd396ca4de9675395ab4
Elena <nezh21@mail.ru>
2019-09-25 10:36:29 +0400

    Delete validation

--- js/script.js
+++ js/script.js
@@ -60,32 +60,6 @@ $(document).ready(function() {
         });
     });
 
-    // Валидация формы jQuery Validation
-    $('.contacts form').validate({
-        // rules: {
-        //     name: {
-        //     required: true,
-        //     minlength: 2,
-        //     }
-
-        //   },
-        //   messages: {
-        //     name: {
-        //         required: "Поле обязательно к заполнению",
-        //         minlength: "Введите не менее 2-х символов"
-        //         },
-        //         email: {
-        //         required: "Поле обязательно к заполнению",
-        //         pattern: "Введите корректный адрес электронной почты",
-        //         },
-        //         text: "Поле обязательно к заполнению"
-        //     }
-    });
-
-    $.validator.methods.pattern = function(value, element) {
-        return (this.optional(element) || new RegExp(element.pattern).test(value));
-    };
-
     // Оповещение после отправки формы
     $('.contacts .btn').click(function(e) {
         e.preventDefault();
